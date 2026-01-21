fbff99ebdfa194d0e9de59f52644fe699152d2f6
GuGuMur <2221533105@qq.com>
2026-01-21 16:12:14 +0800

    chore: remove tempfile

--- src/trapstage/local.py
+++
@@ -1,9 +0,0 @@
-import json
-from pathlib import Path
-
-j = json.loads(Path(Path(__file__).parent / "trapsformat.json").read_text(encoding="utf-8"))
-
-for k, v in j.items():
-    j[k]["settings"] = {}
-
-Path(Path(__file__).parent / "trapsformat.json").write_text(json.dumps(j, ensure_ascii=False, indent=4), encoding="utf-8")
